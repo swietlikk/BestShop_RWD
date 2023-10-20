85d79ebc92a7a270ff19dc979581ce21e15d3565
swietlikk <adamos.swietlicki@gmail.com>
2023-10-20 14:09:50 +0200

    add some Dom API

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,28 @@
-const formRefs = document.querySelectorAll('.calc__form');
+const formRefs = document.querySelector('.calc__form');
+const itemRefs = document.querySelectorAll('.list__item');
 
+formRefs.addEventListener("input", (event) => {
+    const target = event.target;
+    const products = document.querySelector('[data-id="products"]');
+    const orders = document.querySelector('[data-id="orders"]');
+    const accounting = document.querySelector('[data-id="accounting"]');
+    const terminal = document.querySelector('[data-id="terminal"]');
 
-formRefs.forEach((ref) => {
-    ref.addEventListener('click', (event) => {
-        event.target.classList = '#products'
-    })
-})
+    if (target.id === 'products') {
+        products.classList.add('open');
+        const value = parseFloat(target.value) || 0;
+        const price = value * 0.5;
+        products.querySelector('.item__calc').textContent = `${value} * $0.5`;
+        products.querySelector('.item__price').textContent = `$${price}`;
+    } else if (target.id === 'orders') {
+        orders.classList.add('open');
+        const value = parseFloat(target.value) || 0;
+        const price = value * 0.5;
+        orders.querySelector('.item__calc').textContent = `${value} * $0.5`;
+        orders.querySelector('.item__price').textContent = `$${price}`;
+    } else if (target.id === 'accounting') {
+        accounting.classList.toggle('open');
+    } else if (target.id === 'terminal') {
+        terminal.classList.toggle('open');
+    }
+});
