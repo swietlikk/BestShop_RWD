5af499a351eed25dafb323f0d9daedfc168fd314
swietlikk <adamos.swietlicki@gmail.com>
2023-10-20 16:47:07 +0200

    add js for calculator section

--- dist/index.8f0c9192.js
+++ dist/index.8f0c9192.js
@@ -1,25 +1,36 @@
 const formRefs = document.querySelector(".calc__form");
-const itemRefs = document.querySelectorAll(".list__item");
+const selectRef = document.getElementById("package");
+const dropdownRef = document.querySelector(".calc__select");
+const itemsRefs = [
+    ...document.querySelectorAll(".select__dropdown")
+];
+console.log(dropdownRef);
+function doAwesomeThings(items, target) {
+    items.classList.add("open");
+    const value = parseFloat(target.value) || 0;
+    const price = value * 0.5;
+    items.querySelector(".item__calc").textContent = `${value} * $0.5`;
+    items.querySelector(".item__price").textContent = `$${price}`;
+}
 formRefs.addEventListener("input", (event)=>{
     const target = event.target;
     const products = document.querySelector('[data-id="products"]');
     const orders = document.querySelector('[data-id="orders"]');
     const accounting = document.querySelector('[data-id="accounting"]');
     const terminal = document.querySelector('[data-id="terminal"]');
-    if (target.id === "products") {
-        products.classList.add("open");
-        const value = parseFloat(target.value) || 0;
-        const price = value * 0.5;
-        products.querySelector(".item__calc").textContent = `${value} * $0.5`;
-        products.querySelector(".item__price").textContent = `$${price}`;
-    } else if (target.id === "orders") {
-        orders.classList.add("open");
-        const value = parseFloat(target.value) || 0;
-        const price = value * 0.5;
-        orders.querySelector(".item__calc").textContent = `${value} * $0.5`;
-        orders.querySelector(".item__price").textContent = `$${price}`;
-    } else if (target.id === "accounting") accounting.classList.toggle("open");
+    if (target.id === "products") doAwesomeThings(products, target);
+    else if (target.id === "orders") doAwesomeThings(orders, target);
+    else if (target.id === "accounting") accounting.classList.toggle("open");
     else if (target.id === "terminal") terminal.classList.toggle("open");
+});
+selectRef.addEventListener("click", ()=>{
+    dropdownRef.classList.toggle("open");
+    console.log(dropdownRef);
+}, false);
+itemsRefs.forEach((item)=>{
+    item.addEventListener("click", (event)=>{
+        document.querySelector("[data-id='package']").classList.toggle("open");
+    }, true);
 });
 
 //# sourceMappingURL=index.8f0c9192.js.map
--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,19 @@
 const formRefs = document.querySelector('.calc__form');
-const itemRefs = document.querySelectorAll('.list__item');
+const selectRef = document.getElementById('package');
+const dropdownRef = document.querySelector('.calc__select');
+const itemsRefs = [...document.querySelectorAll('.select__dropdown')];
+
+
+console.log(dropdownRef);
+
+
+function doAwesomeThings(items, target) {
+    items.classList.add('open');
+    const value = parseFloat(target.value) || 0;
+    const price = value * 0.5;
+    items.querySelector('.item__calc').textContent = `${value} * $0.5`;
+    items.querySelector('.item__price').textContent = `$${price}`;
+}
 
 formRefs.addEventListener("input", (event) => {
     const target = event.target;
@@ -9,20 +23,24 @@ formRefs.addEventListener("input", (event) => {
     const terminal = document.querySelector('[data-id="terminal"]');
 
     if (target.id === 'products') {
-        products.classList.add('open');
-        const value = parseFloat(target.value) || 0;
-        const price = value * 0.5;
-        products.querySelector('.item__calc').textContent = `${value} * $0.5`;
-        products.querySelector('.item__price').textContent = `$${price}`;
+        doAwesomeThings(products, target);
     } else if (target.id === 'orders') {
-        orders.classList.add('open');
-        const value = parseFloat(target.value) || 0;
-        const price = value * 0.5;
-        orders.querySelector('.item__calc').textContent = `${value} * $0.5`;
-        orders.querySelector('.item__price').textContent = `$${price}`;
+        doAwesomeThings(orders, target);
     } else if (target.id === 'accounting') {
         accounting.classList.toggle('open');
     } else if (target.id === 'terminal') {
         terminal.classList.toggle('open');
     }
 });
+
+selectRef.addEventListener('click', () => {
+    dropdownRef.classList.toggle('open');
+    console.log(dropdownRef)
+}, false);
+
+itemsRefs.forEach((item) => {
+    item.addEventListener('click', (event) => {
+        document.querySelector("[data-id='package']").classList.toggle('open');
+
+    }, true)
+})
